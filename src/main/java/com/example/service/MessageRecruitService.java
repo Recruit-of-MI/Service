fdeb3550fce0e534fb4b28770098e374b6b9049f
hzy <2661243013@qq.com>
2022-05-09 18:54:02 +0800

    完善消息列表

--- src/main/java/com/example/service/MessageRecruitService.java
+++ src/main/java/com/example/service/MessageRecruitService.java
@@ -3,7 +3,9 @@ package com.example.service;
 import com.example.bean.MessageRecruit;
 import com.example.mapper.MessageRecruitMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MessageRecruitService {
     @Autowired
     MessageRecruitMapper messageRecruitMapper;
